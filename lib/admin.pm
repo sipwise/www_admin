889b451e00760a74a9b26b7c5bf20c77fecf8918
Daniel Tiefnig <dtiefnig@sipwise.com>
2009-05-18 00:26:09 +0000

    creating new branch 1.2 from trunk

--- lib/admin.pm
+++ lib/admin.pm
@@ -21,7 +21,7 @@ use Catalyst qw/-Debug ConfigLoader Static::Simple
                 Session Session::Store::FastMmap Session::State::Cookie
                /;
 
-our $VERSION = '1';
+our $VERSION = '1.2';
 
 # Configure the application. 
 #
