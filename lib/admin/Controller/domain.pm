c1ab8bf332221004222f74869e3b2cee620f244f
Daniel Tiefnig <dtiefnig@sipwise.com>
2010-11-17 14:54:35 +0000

    removed local, cc and timezone settings from domains

--- lib/admin/Controller/domain.pm
+++ lib/admin/Controller/domain.pm
@@ -31,69 +31,12 @@ sub index : Private {
                                                       );
     $c->stash->{domains} = $domains if eval { @$domains };
 
-    $c->stash->{edit_domain} = $c->request->params->{edit_domain};
-
-    if(ref $c->session->{restore_domedit_input} eq 'HASH') {
-        foreach my $domain (@{$c->stash->{domains}}) {
-            next unless $$domain{domain} eq $c->stash->{edit_domain};
-            $domain = { %$domain, %{$c->session->{restore_domedit_input}} };
-            last;
-        }
-        delete $c->session->{restore_domedit_input};
-    }
     if(ref $c->session->{restore_domadd_input} eq 'HASH') {
         $c->stash->{arefill} = $c->session->{restore_domadd_input};
         delete $c->session->{restore_domadd_input};
     }
 
     return 1;
-}
-
-=head2 do_edit_domain
-
-Change settings for a domain.
-
-=cut
-
-sub do_edit_domain : Local {
-    my ( $self, $c ) = @_;
-
-    my %messages;
-    my %settings;
-
-    my $domain = $c->request->params->{domain};
-
-    $settings{local} = $c->request->params->{local} ? 1 : 0;
-
-    $settings{cc} = $c->request->params->{cc};
-    $messages{ecc} = 'Client.Voip.MalformedCc'
-        unless $settings{cc} =~ /^\d+$/;
-
-    $settings{timezone} = $c->request->params->{timezone};
-    $messages{etimezone} = 'Client.Syntax.MalformedTimezone'
-        unless $settings{timezone} =~ m#^\w+/\w.+$#;
-
-    unless(keys %messages) {
-        if($c->model('Provisioning')->call_prov( $c, 'billing', 'update_domain',
-                                                 { domain => $domain,
-                                                   data   => \%settings,
-                                                 },
-                                                 undef
-                                               ))
-        {
-            $messages{edommsg} = 'Server.Voip.SavedSettings';
-            $c->session->{messages} = \%messages;
-            $c->response->redirect("/domain");
-            return;
-        }
-    } else {
-        $messages{edomerr} = 'Client.Voip.InputErrorFound';
-    }
-
-    $c->session->{messages} = \%messages;
-    $c->session->{restore_domedit_input} = \%settings;
-    $c->response->redirect("/domain?edit_domain=$domain");
-    return;
 }
 
 =head2 do_create_domain
@@ -106,25 +49,12 @@ sub do_create_domain : Local {
     my ( $self, $c ) = @_;
 
     my %messages;
-    my %settings;
 
     my $domain = $c->request->params->{domain};
 
-    $settings{local} = $c->request->params->{local} ? 1 : 0;
-
-    $settings{cc} = $c->request->params->{cc};
-    $messages{acc} = 'Client.Voip.MalformedCc'
-        unless $settings{cc} =~ /^\d+$/;
-
-    $settings{timezone} = $c->request->params->{timezone};
-    $messages{atimezone} = 'Client.Syntax.MalformedTimezone'
-        unless $settings{timezone} =~ m#^\w+/\w.+$#;
-
     unless(keys %messages) {
         if($c->model('Provisioning')->call_prov( $c, 'billing', 'create_domain',
-                                                 { domain => $domain,
-                                                   data   => \%settings,
-                                                 },
+                                                 { domain => $domain },
                                                  undef
                                                ))
         {
@@ -138,7 +68,6 @@ sub do_create_domain : Local {
     }
 
     $c->session->{messages} = \%messages;
-    $c->session->{restore_domadd_input} = \%settings;
     $c->session->{restore_domadd_input}{domain} = $domain;
     $c->response->redirect("/domain");
     return;
